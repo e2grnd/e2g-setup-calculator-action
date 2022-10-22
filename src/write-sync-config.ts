451a809a78452a07ff341d96b7a477c3ee3ff8fa
Aaron Lampros <alampros@gmail.com>
2022-10-21 21:54:56 -0400

    add write repo sync config script will use this to keep the sync script updated in https://github.com/e2grnd/calculator-template/blob/release/dev/.github/sync.yml

---
+++ src/write-sync-config.ts
@@ -0,0 +1,32 @@
+import fsAll from 'fs'
+import {calculators} from './calculators'
+
+const fs = fsAll.promises
+
+async function run(): Promise<void> {
+  const sortedRepos = Object.entries(calculators)
+    .sort((a, b) => {
+      return a[1].serviceName.localeCompare(b[1].serviceName)
+    })
+    .map(([repoName]) => `e2grnd/${repoName}@release/dev`)
+  const src = `group:
+  repos: |
+    ${sortedRepos.join('\n    ')}
+  files: 
+    - source: .github/workflows/publish.yml
+      dest: .github/workflows/publish.yml
+    - source: .devcontainer/docker-compose.yaml
+      dest: .devcontainer/docker-compose.yaml
+    - source: .devcontainer/docker-compose.app.yaml
+      dest: .devcontainer/docker-compose.app.yaml
+    - source: .devcontainer/devcontainer.json
+      dest: .devcontainer/devcontainer.json
+    - source: .devcontainer/scripts/init.mjs
+      dest: .devcontainer/scripts/init.mjs
+
+`
+
+  await fs.writeFile('repo-sync-config.yaml', src)
+}
+
+run()
