f74d85da98a769153f54b2068e1c9b68140eb87f
Aaron Lampros <alampros@gmail.com>
2022-11-29 21:09:08 -0500

    add script to restart all calc deployments

---
+++ src/write-rollout-restart.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import fsAll from 'fs'
+import {calculators} from './calculators'
+
+const fs = fsAll.promises
+
+async function run(): Promise<void> {
+  const calcs = Object.entries(calculators)
+    .sort((a, b) => {
+      return a[1].serviceName.localeCompare(b[1].serviceName)
+    })
+    .map(([repoName, calcConfig]) => calcConfig.serviceName)
+  const scr = `#!/bin/bash
+${calcs.map(sn => `kubectl rollout restart -n dev deployment/${sn}`).join('\n')}
+`
+  await fs.mkdir('etc', {recursive: true})
+  await fs.writeFile('etc/rollout-restart.sh', scr)
+}
+
+run()
